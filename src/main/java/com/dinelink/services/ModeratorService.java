0fe172906d254551ceb92043a41af143ba25649f
Aryan Agrawal <aryanagr219@gmail.com>
2025-02-23 20:32:17 +0530

    Feat : Save moderator api implemented

--- src/main/java/com/dinelink/services/ModeratorService.java
+++ src/main/java/com/dinelink/services/ModeratorService.java
@@ -1,10 +1,10 @@
 package com.dinelink.services;
 
-import com.dinelink.entities.LoginRequest;
-import com.dinelink.entities.Moderator;
+import com.dinelink.entities.*;
 import com.dinelink.exceptions.EmailOrPasswordIncorrectException;
 import com.dinelink.exceptions.ModeratorNotFoundException;
 import com.dinelink.repositories.ModeratorRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,6 +21,30 @@ public class ModeratorService {
         this.moderatorRepository = moderatorRepository;
     }
 
+    public ModeratorResponse saveModerator(ModeratorRequest moderatorRequest) throws Exception {
+        Moderator m = moderatorRepository.findByEmail(moderatorRequest.getEmail());
+        if (m != null) {
+            throw new Exception("A Moderator already exists with this email");
+        }
+        String name = moderatorRequest.getName();
+        String email = moderatorRequest.getEmail();
+        String password = moderatorRequest.getPassword();
+        String roleReq = moderatorRequest.getRole();
+        Role role;
+        if (roleReq.equalsIgnoreCase("chef")) {
+            role = Role.CHEF;
+        } else if(roleReq.equalsIgnoreCase("admin")){
+            role = Role.ADMIN;
+        }else{
+            throw new Exception("Invalid Moderator Role");
+        }
+        Moderator moderator = new Moderator(email, password, name, role);
+
+        Moderator dbModerator = moderatorRepository.save(moderator);
+        ModeratorResponse moderatorResponse = new ModeratorResponse();
+        BeanUtils.copyProperties(dbModerator, moderatorResponse);
+        return moderatorResponse;
+    }
 
     public Moderator login(LoginRequest loginRequest) {
         String email = loginRequest.getEmail();
